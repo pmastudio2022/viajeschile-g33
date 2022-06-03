cc9b0d39aa4cfa7f80d829f854d8d09cf59e385e
pablomontt18@gmail.com <pablomonttamenabar@MacBook-Pro-de-Pablo.local>
2022-06-03 17:58:35 -0400

    Tooltip

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,6 @@ $(document).scroll(function() {
     const y = $("html").scrollTop();
     y > 300 ? $("nav").addClass("nav-black") : $("nav").removeClass("nav-black")
 });
+$(function(){
+    $('[data-bs-toggle="tooltip"]').tooltip ();
+})
